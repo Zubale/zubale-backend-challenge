b075050eba1e5304698c61ddc4d040b35897af39
Cosme Escobedo <comeoes@gmail.com>
2019-05-01 11:20:50 -0600

    Created the backbone for QuotesController
    -Changed the controller mapping to /api/quotes
    -Changed the / mapping function name to getAllQuotes which will return
    in all the qoutes ordered by number of votes
    -Added a /submit mapping that takes as a parameter quoteText
    -Added a /votedown and /voteup mappings to vote qoutes
    -Added a /list mapping to list quotes by page ordered by number of votes

--- src/main/kotlin/com/pulpos/quotes/HttpControllers.kt
+++ src/main/kotlin/com/pulpos/quotes/HttpControllers.kt
@@ -1,12 +1,34 @@
 package com.pulpos.quotes
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/quotes")
 class QuotesController {
     @GetMapping("/")
-    fun helloWorld() : String {
-        return "Hello world"
+    fun getAllQuotes() : String {
+        return "Not implemented yet"
     }
+
+    @PostMapping("/submit")
+    fun submit(@RequestParam quoteText : String) : String {
+        return "Not implemented yet"
+    }
+
+    @GetMapping("/votedown")
+    fun voteUp(@RequestParam(name = "id") quoteId : Int) : String {
+        return "Not implemented yet"
+    }
+
+    @GetMapping("/votedown")
+    fun voteDown(@RequestParam(name = "id") quoteId : Int) : String {
+        return "Not implemented yet"
+    }
+
+    @GetMapping("/list")
+    fun getQuotePage(@RequestParam page : Int, @RequestParam pageSize : Int) : String {
+        return "Not inplemented yet"
+    }
+
+
 }
