5b140b18e3c865d977a9d4e8fd1f36955d3a1a41
Cosme Escobedo <comeoes@gmail.com>
2019-05-11 16:40:34 -0600

    Changed the submit method in QuotesController
    -Now the submit method creates a quote and sets the user that created it
    to a default one
    -If the author isn't sent in the request or is empty it will be set to
    the poster's username

--- src/main/kotlin/com/pulpos/quotes/HttpControllers.kt
+++ src/main/kotlin/com/pulpos/quotes/HttpControllers.kt
@@ -1,23 +1,32 @@
 package com.pulpos.quotes
 
 import com.pulpos.quotes.model.Quote
+import com.pulpos.quotes.model.User
 import com.pulpos.quotes.repository.QuoteRepository
+import com.pulpos.quotes.repository.UserRepository
 import org.springframework.web.bind.annotation.*
 import org.springframework.http.ResponseEntity
 import java.util.*
 import javax.validation.Valid
 import org.springframework.http.HttpStatus
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/api/quotes")
-class QuotesController(private val quoteRepository: QuoteRepository) {
+class QuotesController(private val quoteRepository: QuoteRepository, private val userRepository: UserRepository) {
     @GetMapping("/")
     fun getAllQuotes() : List<Quote> =
         quoteRepository.findAll()
 
     @PostMapping("/submit")
-    fun submit(@Valid @RequestBody quote : Quote) : Quote =
-        quoteRepository.save(quote)
+    fun submit(@Valid @RequestBody quote : Quote) : Quote {
+        val user : User = userRepository.findById(1).get() //TODO: This user should be obtained using authentication
+        val author = if (quote.author.isBlank()) user.username else quote.author
+        val quoteToSave = Quote(text = quote.text, author =  author, date = LocalDate.now(), user = user)
+
+        return quoteRepository.save(quoteToSave)
+    }
+
     
 
     @GetMapping("/voteup")
